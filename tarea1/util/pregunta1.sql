e9e240943d6083a32cdd1320bb8f68465e4227ea
Jonathan Gonzalez <jgonzalez@codesa.co.cr>
2020-04-25 22:56:38 -0600

    pregunta 1

---
+++ tarea1/util/pregunta1.sql
@@ -0,0 +1,35 @@
+-- Analisis por Ingresos ultimos 2 años de productos
+SELECT dimP.*, sum(precioVenta) as TotalVenta
+FROM tarea1.dimordenes dimO 
+	 INNER JOIN tarea1.hechosordenes ho ON dimO.idOrden = ho.idDimOrden
+     LEFT JOIN tarea1.dimproductos dimP ON idDimProducto = dimP.idProducto
+WHERE dimO.fechaOrden >= DATE_SUB(NOW(), INTERVAL 2 YEAR)
+GROUP BY dimP.idProducto
+ORDER BY TotalVenta DESC;
+
+-- Analisis por Ingresos ultimos 2 años por marcas
+SELECT dimP.nomMarca, sum(precioVenta) as TotalVenta
+FROM tarea1.dimordenes dimO 
+	 INNER JOIN tarea1.hechosordenes ho ON dimO.idOrden = ho.idDimOrden
+     LEFT JOIN tarea1.dimproductos dimP ON idDimProducto = dimP.idProducto
+WHERE dimO.fechaOrden >= DATE_SUB(NOW(), INTERVAL 2 YEAR)
+GROUP BY dimP.nomMarca
+ORDER BY TotalVenta DESC;
+
+-- Analisis por Ingresos ultimos 2 años por modelo
+SELECT dimP.annoModelo, sum(precioVenta) as TotalVenta
+FROM tarea1.dimordenes dimO 
+	 INNER JOIN tarea1.hechosordenes ho ON dimO.idOrden = ho.idDimOrden
+     LEFT JOIN tarea1.dimproductos dimP ON idDimProducto = dimP.idProducto
+WHERE dimO.fechaOrden >= DATE_SUB(NOW(), INTERVAL 2 YEAR)
+GROUP BY dimP.annoModelo
+ORDER BY TotalVenta DESC;
+
+-- Analisis por Ingresos ultimos 2 años por marcas y modelo
+SELECT dimP.nomMarca, dimP.annoModelo, sum(precioVenta) as TotalVenta
+FROM tarea1.dimordenes dimO 
+	 INNER JOIN tarea1.hechosordenes ho ON dimO.idOrden = ho.idDimOrden
+     LEFT JOIN tarea1.dimproductos dimP ON idDimProducto = dimP.idProducto
+WHERE dimO.fechaOrden >= DATE_SUB(NOW(), INTERVAL 2 YEAR)
+GROUP BY dimP.nomMarca, dimP.annoModelo
+ORDER BY TotalVenta DESC;
